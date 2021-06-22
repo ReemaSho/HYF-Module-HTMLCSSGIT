8a422aefa83487c75a8a1d13b0629d5492c30ed0
Reema <Reemaalshohof88@gmail.com>
2021-06-22 18:17:35 +0200

    add slider section

--- week3/patreon/script.js
+++ week3/patreon/script.js
@@ -9,12 +9,27 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     var instances = M.Dropdown.init(down , options);
     });
+    // sidenavbar
 
     document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('.sidenav');
         var instances = M.Sidenav.init(elems, {});
       });
 
+// close sidenavbar
 function closeBtn () {
         document.getElementById('mobile-demo').style.display='none';
-    }
+    };
+
+// slider
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.slider');
+        var options ={
+            indicators : false ,
+            duration : 400 , 
+            interval : 4000 ,
+        }
+        var instances = M.Slider.init(elems, options);
+      });
+
+    
